4b5ece33c564aef1b124e296288b8f817fe9412f
Inpyo-Jeon <inpyo.developer@gmail.com>
2019-07-02 02:58:42 +0900

    1. 검색에 대한 Table, Repository, Service 기본 뼈대 추가

---
+++ src/main/java/com/food/chicken/repository/SearchHistoryRepository.java
@@ -0,0 +1,11 @@
+package com.food.chicken.repository;
+
+import com.food.chicken.model.SearchHistory;
+import com.food.chicken.model.SearchHistoryPK;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface SearchHistoryRepository extends CrudRepository<SearchHistory, SearchHistoryPK> {
+
+}
