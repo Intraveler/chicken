e169c04fe8ba824263c25bf8c2bc7ae10f9168aa
Inpyo-Jeon <inpyo.developer@gmail.com>
2019-07-02 01:45:27 +0900

    1. 회원 관련 Table과 Repository 생성

---
+++ src/main/java/com/food/chicken/repository/MemberRepository.java
@@ -0,0 +1,9 @@
+package com.food.chicken.repository;
+
+import com.food.chicken.model.Member;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface MemberRepository extends CrudRepository<Member, Long> {
+}
